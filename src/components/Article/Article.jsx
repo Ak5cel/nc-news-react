c4620c66dce66318d379c90b1b337f58ff83678e
Ak5cel <akhilaficel@gmail.com>
2023-12-13 15:03:59 +0530

    add CommentsSection to SingleArticleView

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -29,9 +29,7 @@ const Article = ({ article_id }) => {
       <p className="topic-tag">{article.topic}</p>
       <img src={article.article_img_url} alt="" />
       <p className="article-body">{article.body}</p>
-      <p>
-        {article.votes} votes • {article.comment_count} comments
-      </p>
+      <p>{article.votes} votes</p>
     </article>
   );
 };
