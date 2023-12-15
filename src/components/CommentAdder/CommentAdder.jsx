f770f9fd95537075806d8337d5485d711ad513a8
Ak5cel <akhilaficel@gmail.com>
2023-12-15 15:18:41 +0530

    make posting comments optimistic, add error feedback

--- src/components/CommentAdder/CommentAdder.jsx
+++ src/components/CommentAdder/CommentAdder.jsx
@@ -1,12 +1,22 @@
 import "./CommentAdder.css";
 import { UserContext } from "../../contexts/UserContext";
 import { useContext, useRef, useState } from "react";
+import { useSnackbar } from "notistack";
 import { postComment } from "../../utils/api";
 
 const CommentAdder = ({ setComments, article_id }) => {
   const { currentUser } = useContext(UserContext);
   const [commentBodyInput, setCommentBodyInput] = useState("");
   const addButtonRef = useRef(null);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const showCustomError = (err) => {
+    if (err.code === "ERR_NETWORK") {
+      enqueueSnackbar("Network error. Please make sure you're online and try again");
+    } else {
+      enqueueSnackbar("Oops, something went wrong");
+    }
+  };
 
   const handleCommentInput = (e) => {
     setCommentBodyInput(e.target.value);
@@ -14,14 +24,32 @@ const CommentAdder = ({ setComments, article_id }) => {
 
   const handleAddComment = (e) => {
     e.preventDefault();
-    addButtonRef.current.disabled = true; // disable button temporarily until posted
 
-    postComment(currentUser, commentBodyInput, article_id).then((newComment) => {
+    setComments((currComments) => {
+      const newComment = {
+        comment_id: currComments.length + 1,
+        body: commentBodyInput,
+        article_id: article_id,
+        author: currentUser,
+        votes: 0,
+        created_at: Date.now(),
+      };
+
+      return [newComment, ...currComments];
+    });
+    setCommentBodyInput("");
+
+    postComment(currentUser, commentBodyInput, article_id).catch((err) => {
+      // reset comments and refill the form with user's input
+      // if posting fails
+      setCommentBodyInput(commentBodyInput);
       setComments((currComments) => {
-        return [newComment, ...currComments];
+        const newCommentId = currComments.length;
+        console.log(newCommentId, "<--new id");
+
+        return currComments.filter((comment) => comment.comment_id !== newCommentId);
       });
-      setCommentBodyInput("");
-      addButtonRef.current.disabled = false;
+      showCustomError(err);
     });
   };
 
